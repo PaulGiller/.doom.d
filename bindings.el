be19529ae8cfdcc05a71b3cefa42894a4ff26485
Paul Giller <paulgiller@gmail.com>
2018-08-14 21:33:05 +0100

    Working mu4e config.

---
+++ bindings.el
@@ -0,0 +1,5 @@
+;;; ~/.doom.d/+bindings.el -*- lexical-binding: t; -*-
+
+(map! :leader
+      (:desc "open" :prefix "o"
+        :desc "Mail" :n "m" #'=email))
--- config.el
+++ config.el
@@ -1,9 +1,13 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 ;; Example: https://gist.github.com/hjertnes/9d1ceecb403b3bad1764b41c4ca284b7
+;;; Code:
+(when (featurep! :feature evil)
+  (load! "bindings"))
 
 (after! mu4e
   (setq mu4e-maildir            "~/Maildir"
         mu4e-get-mail-command   "offlineimap -o -q"
+        mu4e-use-fancy-chars    t
         mu4e-attachment-dir     "~/Maildir/.attachments"
         mu4e-sent-folder        "/[Google Mail].Sent Mail"
         mu4e-drafts-folder      "/[Google Mail].Drafts"
@@ -17,9 +21,21 @@
         '(("smtp.gmail.com" 587 "paulgiller@gmail.com" nil))
         smtpmail-default-smtp-server "smtp.gmail.com"
         smtpmail-smtp-server "smtp.gmail.com"
+        mu4e-headers-fields '((:account . 24)
+                              (:human-date . 12)
+                              (:flags . 4)
+                              (:from . 25)
+                              (:subject))
         smtpmail-smtp-service 587
         mu4e-maildir-shortcuts '( ("/INBOX"               . ?i)
                                   ("/[Google Mail].Sent Mail"   . ?s)
                                   ("/[Google Mail].Trash"       . ?t)
                                   ("/[Google Mail].All Mail"    . ?a))))
 
+  (defun htmlize-before-send ()
+    "When in an org-mu4e-compose-org-mode message, htmlize it."
+    (when (member 'org~mu4e-mime-switch-headers-or-body post-command-hook)
+      (message-mode)
+      (org-mime-htmlize)))
+
+  (advice-add 'message-send-and-exit :before 'htmlize-before-send)
