ce4803a2643bf9cb3af9a54a6c650412a6e13e20
Paul Giller <paulgiller@gmail.com>
2018-08-18 00:34:19 +0100

    Added Org-Brain

--- config.el
+++ config.el
@@ -54,5 +54,17 @@
  (set-popup-rule! "^\\*cider-repl" :quit 'current :select nil :width 85 :side 'right :slot 1)
  (set-popup-rule! "^\\*cider-error" :quit 'current :select t :height 0.5 :side 'right :slot 2))
 
-
-
+(def-package! org-brain; :ensure t
+ :init
+ (setq org-brain-path "~/org/brain")
+ ;; For Evil users (with-eval-after-load 'evil
+   (evil-set-initial-state 'org-brain-visualize-mode 'emacs)
+ :config
+ (setq org-id-track-globally t)
+ (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+ (push '("b" "Brain" plain (function org-brain-goto-end)
+         "* %i%?" :empty-lines 1)
+       org-capture-templates)
+ (setq org-brain-visualize-default-choices 'all)
+ (setq org-brain-title-max-length 12)
+ (set-popup-rule! "^\\*org-brain" :ignore t))
--- packages.el
+++ packages.el
@@ -2,3 +2,4 @@
 ;;; ~/.doom.d/packages.el
 (package! org-mime)
 (package! ace-jump-mode)
+(package! org-brain)
