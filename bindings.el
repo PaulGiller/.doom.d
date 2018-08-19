966497da9545e56625bf349dc5b4e188e40aea44
Paul Giller <paulgiller@gmail.com>
2018-08-19 11:59:15 +0100

    Added Visualiser key-binding

--- bindings.el
+++ bindings.el
@@ -4,7 +4,8 @@
  :gnvime "C-<f1>" #'ace-jump-mode
  :leader
       (:desc "open" :prefix "o"
-        :desc "Mail" :n "m" #'=email)
+       :desc "Mail" :n "m" #'=email
+       :desc "Org-Brain Visualiser" :n "v" #'org-brain-visualize)
 
       (:desc "insert" :prefix "i"
         :desc "ClipLink" :nv "c" #'org-brain-cliplink-resource))
